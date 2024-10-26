ab2f47fdbf8bac2b336c0fb6d3e2e3b9ed83124c
Dhanush N Prasad <dhanushn@www.trackoworld.com>
2024-10-26 15:54:31 +0530

    updated Patient Profile and Patient Appointment

--- apps/patient/urls.py
+++ apps/patient/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import PatientAppointment
+from .views import PatientAppointment,PatientProfile
 urlpatterns = [
     path('p_apoint/',PatientAppointment.as_view(),name='PatientAppointment'),
+    path('p_profile/',PatientProfile.as_view(),name='p_profile'),
 ]
--- apps/patient/views.py
+++ apps/patient/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from apps.autenticateapp.models import AppointmentModel
-from apps.autenticateapp.serializers import AppointmentSerials
+from apps.autenticateapp.models import AppointmentModel,PatientModel
+from apps.autenticateapp.serializers import AppointmentSerials,PatientRegSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from apps.autenticateapp.serializers import AppointmentSerials
@@ -14,9 +14,25 @@ class PatientAppointment(APIView):
     def get(self,request,*args, **kwargs):
         id = request.user.username 
         obj  = AppointmentModel.objects.filter(user__username=id)
-        print(obj[0])
-        serial = AppointmentSerials(obj)
-        
+        serial = AppointmentSerials(obj,many=True)
         return Response({
             'data': serial.data
         },status=status.HTTP_200_OK)
+    
+
+class PatientProfile(APIView):
+    
+    def get(self,request,*args, **kwargs):
+        try:
+            
+            user = request.user.username
+            obj = PatientModel.objects.get(user__username = user)
+            serial = PatientRegSerializer(obj)
+            
+            return Response({
+                'data' : serial.data
+            },status=status.HTTP_200_OK)
+        except PatientModel.DoesNotExist:
+            return Response({
+                'data' : 'Patient Does Not Exist'
+            },status=status.HTTP_404_NOT_FOUND)
